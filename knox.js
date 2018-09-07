9f1f4942fc4676630be94b44badac3b3c9162924
nanlirkapnan <nanlirkapnan@gmail.com>
2018-09-07 20:21:33 +0100

    my first commit

---
+++ knox.js
@@ -0,0 +1,8 @@
+function firstCode(str, input_name) {
+    var message ="Welcome here";
+  	var input_name = " Knox";
+    message += input_name;
+    return message;
+}
+
+console.log(firstCode());
